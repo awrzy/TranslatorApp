5bcbdc9955ff71a88f7fe2e5cb34bc32042b7266
yuuo <yuuo@DESKTOP-NV8RV3L>
2025-03-05 09:28:19 +0300

    Добавьте файлы проекта.

---
+++ googletranslate/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace TranslatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TextBoxInput = new System.Windows.Forms.TextBox();
+            TextBoxOutput = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            ComboBoxFrom = new System.Windows.Forms.ComboBox();
+            ComboBoxTo = new System.Windows.Forms.ComboBox();
+            SuspendLayout();
+            // 
+            // TextBoxInput
+            // 
+            TextBoxInput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            TextBoxInput.Location = new System.Drawing.Point(103, 102);
+            TextBoxInput.Multiline = true;
+            TextBoxInput.Name = "TextBoxInput";
+            TextBoxInput.Size = new System.Drawing.Size(279, 226);
+            TextBoxInput.TabIndex = 6;
+            // 
+            // TextBoxOutput
+            // 
+            TextBoxOutput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            TextBoxOutput.Location = new System.Drawing.Point(480, 102);
+            TextBoxOutput.Multiline = true;
+            TextBoxOutput.Name = "TextBoxOutput";
+            TextBoxOutput.Size = new System.Drawing.Size(279, 226);
+            TextBoxOutput.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(397, 357);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Перевести";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // ComboBoxFrom
+            // 
+            ComboBoxFrom.FormattingEnabled = true;
+            ComboBoxFrom.Location = new System.Drawing.Point(252, 295);
+            ComboBoxFrom.Name = "ComboBoxFrom";
+            ComboBoxFrom.Size = new System.Drawing.Size(121, 23);
+            ComboBoxFrom.TabIndex = 9;
+            // 
+            // ComboBoxTo
+            // 
+            ComboBoxTo.FormattingEnabled = true;
+            ComboBoxTo.Location = new System.Drawing.Point(628, 295);
+            ComboBoxTo.Name = "ComboBoxTo";
+            ComboBoxTo.Size = new System.Drawing.Size(121, 23);
+            ComboBoxTo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(829, 450);
+            Controls.Add(ComboBoxTo);
+            Controls.Add(ComboBoxFrom);
+            Controls.Add(button1);
+            Controls.Add(TextBoxOutput);
+            Controls.Add(TextBoxInput);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TextBoxInput;
+        private System.Windows.Forms.TextBox TextBoxOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox ComboBoxFrom;
+        private System.Windows.Forms.ComboBox ComboBoxTo;
+    }
+}
+
---
+++ googletranslate/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+
+namespace TranslatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextBoxOutput.Text = translate(TextBoxInput.Text, ComboBoxFrom.Text, ComboBoxTo.Text);
+        }
+        public String translate(String input, string from, string to)
+        {
+            var fromLanguage = from;
+            var toLanguage = to;
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(input)}";
+            var webclient = new WebClient
+            {
+                Encoding = System.Text.Encoding.UTF8
+            };
+            var result = webclient.DownloadString(url);
+            try
+            {
+                result = result.Substring(4, result.IndexOf("\"", 4
+                    , StringComparison.Ordinal) - 4);
+                return result;
+            }
+            catch (Exception e1)
+            {
+                return "error";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboBoxFrom.Items.AddRange(new string[] { "en","ru", "es", "fr", "de", "zh" });
+            ComboBoxTo.Items.AddRange(new string[] { "en", "ru", "es", "fr", "de", "zh" });
+            ComboBoxFrom.SelectedIndex = 0;
+            ComboBoxTo.SelectedIndex = 1;
+        }
+    }
+}
